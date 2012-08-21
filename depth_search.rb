a128a84a74569a5055c3ce90af6a5fd5497f29d4
Luis Daher <luisotaviodaher@gmail.com>
2012-08-21 16:51:55 -0300

    Update depth_search.rb
    Corrected!

--- depth_search.rb
+++ depth_search.rb
@@ -1,71 +1,27 @@
 #Blind Search (Depth)
-
+@moves_array  = Array.new
 def copy_matrix matrix
   response = []
-  
-  matrix.each do |array|
-    response << Array.new
-    array.each do |value|
-      response[response.size-1] << value
-    end
-  end
-  response  
+  matrix.each{|array| response << array.dup }
+  response 
 end
 
-def copy_moves moves
-  response = []
-  
-  moves.each do |move|
-    response << move
-  end
-  response  
-end
-
-# finds the number of possible exits of a given matrix, considering the "exit" as the bottom left corner of the matrix
-def expand matrix, x, y, level, moves
-  total = 0
+# finds the number of possible exits of a given matrix, considering the "exit" as the bottom right corner of the matrix
+def expand(matrix, x, y, moves = Array.new)
   matrix[x][y] = true
 
-  if x == (matrix.size-1) and y == (matrix[0].size-1)
-   puts "exit found!"
-   puts moves.join ","
-   puts ""
-   return 1
-  end 
+  @moves_array.push(moves) if x == (matrix.size-1) and y == (matrix[0].size-1)
 
-  #down
-  if !matrix[x+1].nil? and matrix[x+1][y] == false
-    updated_moves = copy_moves(moves)
-    updated_moves << "down"
-    total+= expand(copy_matrix(matrix), x+1,y, level+1, updated_moves)
-  end
+  expand(copy_matrix(matrix), x+1,y, moves.dup.push("down")) if !matrix[x+1].nil? and !matrix[x+1][y] and x+1<matrix.size
 
-  #up
-  if !matrix[x-1].nil? and matrix[x-1][y] == false
-    updated_moves = copy_moves(moves)
-    updated_moves << "up"  
-    total+=expand copy_matrix(matrix), x-1,y,level+1, updated_moves
-  end
+  expand(copy_matrix(matrix), x-1,y, moves.dup.push("up")) if !matrix[x-1].nil? and !matrix[x-1][y] and x-1>=0
   
-  #right
-  if !matrix[x][y+1].nil? and matrix[x][y+1] == false
-    updated_moves = copy_moves(moves)
-    updated_moves << "right"  
-    total+=expand copy_matrix(matrix), x,y+1,level+1, updated_moves
-  end
+  expand(copy_matrix(matrix), x,y+1, moves.dup.push("right")) if !matrix[x][y+1].nil? and !matrix[x][y+1] and y+1<matrix[0].size
 
-  #left
-  if !matrix[x][y-1].nil? and matrix[x][y-1] == false
-    updated_moves = copy_moves(moves)
-    updated_moves << "left"  
-    total+=expand copy_matrix(matrix), x,y-1,level+1, updated_moves
-  end
-  
-  return total
-  
+  expand(copy_matrix(matrix), x,y-1, moves.dup.push("left")) if !matrix[x][y-1].nil? and !matrix[x][y-1] and y-1>=0
 end
-
 
 given_matrix = [[false, false, false, false],[false, false, false, false],[false, false, false, false],[false, false, false, false]]
 
-puts "# of possibilities: " +expand(given_matrix, 0, 0, 0, Array.new).to_s
+expand(given_matrix, 0, 0).to_s
+puts @moves_array.uniq.size
